4d9b97fa2543d89a3e1dadcc4d7d95a1063fd88e
Charity Nelima <wa.nelimacharity@gmail.com>
2022-09-23 16:59:07 +0300

    About.js updated

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,13 @@ import { image } from "../data/data";
 
 function About() {
   return <div>About</div>;
+  return (<div id= "about" > 
+  <h2>About</h2>
+  <p> One thing i love reading is Javascript, React</p>
+  
+  <img src="{image}" alt="I made this"></img>
+  </div>
+  );
 }
 
 export default About;
